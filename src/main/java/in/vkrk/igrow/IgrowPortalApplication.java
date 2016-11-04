9b535852b5ebccef153e64c0b46eff39a9bea93b
Ravikiran VK <vkrk077@gmail.com>
2016-11-04 19:19:03 +0530

    IGrow draft project

---
+++ src/main/java/in/vkrk/igrow/IgrowPortalApplication.java
@@ -0,0 +1,12 @@
+package in.vkrk.igrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrowPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrowPortalApplication.class, args);
+	}
+}
